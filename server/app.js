df13a4e6895318b75f0e888d4164dcbd295ac5e7
UjvalMaurya <motuaurpatlukijodi25@gmail.com>
2025-05-02 12:59:15 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/generator', generatorRoutes);
 
-PORT=process.env.PORT||5000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
